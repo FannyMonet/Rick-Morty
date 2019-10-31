3e8b516a130c4fabe4425fabbf6dfe0d1c017f7f
Fanny Monet <monet.f@sfeir.com>
2019-10-31 16:45:49 +0100

    Added cluster to executes n times in parallel the update method

--- scripts/update_item.js
+++ scripts/update_item.js
@@ -1,30 +1,55 @@
 const AWS = require('aws-sdk');
 const config = require('../config/config.js');
 
-AWS.config.update(config.aws_local_config);
+const cluster = require("cluster");
+const numberOfInstances = process.env.NB_INSTANCES || 100;
 
-const documentClient = new AWS.DynamoDB.DocumentClient();
+const updateItem = async () => {
+    AWS.config.update(config.aws_local_config);
 
-const params = updateParams => ({
-    TableName : [config.aws_table_name],
-    Key: { characterId : updateParams.characterId, species: updateParams.species },
-    UpdateExpression: 'set #a = :value',
-    // ConditionExpression: '#a < :MAX',
-    ExpressionAttributeNames: {'#a' : updateParams.attribute},
-    ExpressionAttributeValues: {':value' : updateParams.value}
-});
-const updateParams = i => ({
-    characterId: 1,
-    species: 'Human',
-    attribute: 'status',
-    value: ('Alive' + i)
-})
+    const documentClient = new AWS.DynamoDB.DocumentClient();
 
-// documentClient.update(params(updateParams), (err, data) => console.log(err ? err : data));
-const updateMultipleTimes = count => {
-    for(let i=1; i<=count; i++) {
-        documentClient.update(params(updateParams(i)), (err, data) => console.log(err ? err : updateParams(i).value));
+    const params = updateParams => ({
+        TableName: [config.aws_table_name],
+        Key: { characterId: updateParams.characterId, species: updateParams.species },
+        UpdateExpression: 'set #a = :value',
+        // ConditionExpression: '#a < :MAX',
+        ExpressionAttributeNames: { '#a': updateParams.attribute },
+        ExpressionAttributeValues: { ':value': updateParams.value }
+    });
+
+    const attributesToUpdate = ['name', 'status', 'type', 'gender', 'url'];
+    const updateParams = i => ({
+        characterId: 1,
+        species: 'Human',
+        attribute: [attributesToUpdate[cluster.worker.id % 5]],
+        value: cluster.worker.id
+    });
+
+
+    const promise = new Promise((resolve, reject) => {
+        documentClient.update(params(updateParams(cluster.worker.id)), (err, data) => err ? reject(err) : resolve(updateParams(1)));
+    });
+
+    try {
+        const res = await promise;
+        console.log("COOL", res);
+    } catch (err) {
+        console.log("ERROR", err);
     }
+
+    process.exit(0);
+};
+
+if (cluster.isMaster) {
+    for (let i = 0; i < numberOfInstances; i++) {
+        cluster.fork();
+    }
+    
+    cluster.on("exit", (worker) => {
+        console.log(`Worker ${worker.process.pid} died.`);
+    });
+} else {
+    updateItem();
 }
-updateMultipleTimes(1000)
-module.exports = (updateParams) =>  documentClient.update(params(updateParams), (err, data) => console.log(err ? err : data));
+// module.exports = (updateParams) => documentClient.update(params(updateParams), (err, data) => console.log(err ? err : data));
