03bee0786a1a05d74565846c97aba3d27f212217
Fanny Monet <monet.f@sfeir.com>
2019-11-04 16:01:06 +0100

    (feat): modified getItem method

--- scripts/get_item.js
+++ scripts/get_item.js
@@ -5,14 +5,14 @@ AWS.config.update(config.aws_local_config);
 
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
-const params = (id, species) => ({
-    TableName : [config.aws_table_name],
+const params = (id, name, species, gender, originId, locationId, created) => ({
+    TableName : config.aws_table_name,
     Key: {
         characterId: id,
-        species: species
+        sortingKey: name + '_' + species + '_' + gender + '_' + 
+            originId + '_'  + locationId + '_' + created
     }
 });
+documentClient.get(params(1, 'Rick Sanchez', 'Human', 'Male', '1', '20', '2017-11-04T18:48:46.250Z'), (err, data) => console.log(err ? err : data));
 
-documentClient.get(params(1, 'Human'), (err, data) => console.log(err ? err : data));
-
-module.exports = (id, species) =>  documentClient.get(params(id, species), (err, data) => console.log(err ? err : data));
+module.exports = (id, species) =>  documentClient.get(params(id, name, species, gender, originId, locationId, created), (err, data) => console.log(err ? err : data));
